395c17522f95f74fe83d6b23d4fd30b7733ecc04
LEHEMBRE Nathalie <nathalie.lehembre@allianz.fr>
2020-10-16 13:40:34 +0200

    Projet1 : 1er commit instable

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,60 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
-	
+
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+		List<String> allSymptoms = new ReadSymptomDataFromFile("C:\\Users\\LEHEMBR\\IdeaProjects\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt").GetSymptoms();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		Set<String> symptoms = new HashSet<>(allSymptoms);
 
-			line = reader.readLine();	// get another symptom
+		for (int i = 0; i < allSymptoms.size(); i++) {
+			countSymptom(allSymptoms.get(i));
 		}
-		
+		writeSymptomStats();
+		return;
+
+	}
+
+	private static String countSymptom(String symptom) {
+
+//		int i = 0;	// set i to 0
+//		while (line != null) {
+//			i++;	// increment i
+//			System.out.println("symptom from file: " + line);
+//			if (line.equals("headache")) {
+//				headacheCount++;
+//				System.out.println("number of headaches: " + headacheCount);
+//			}
+//			else if (line.equals("rash")) {
+//				rashCount++;
+//			}
+//			else if (line.contains("pupils")) {
+//				pupilCount++;
+//			}
+//
+//			line = reader.readLine();	// get another symptom
+//		}
+		return symptom;
+	}
+
+	private static void writeSymptomStats() throws IOException {
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
+		FileWriter writer = new FileWriter ("./result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
 	}
+
 }
